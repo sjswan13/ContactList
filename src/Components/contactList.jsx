9cffc866449cc59948cb22c02e160a8054a89b1d
sjswan13 <sjswan13@gmail.com>
2024-02-14 18:20:58 -0800

    components folder

---
+++ src/Components/contactList.jsx
@@ -0,0 +1,24 @@
+import { React } from 'react';
+
+export default function ContactList() { 
+
+  return ( 
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="3">Contact List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+            {
+               // Map 
+             }
+          </tbody>
+        </table>
+    ); 
+}
