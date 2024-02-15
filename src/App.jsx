7ed7c48dd5b77a2a884cb2531146c968d0016192
annieha04 <itsannha@gmail.com>
2024-02-14 21:10:19 -0500

    test comment

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    {/* comment */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
