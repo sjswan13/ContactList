382e5d9730a112bd9cbea8371c839efa044c179e
sjswan13 <sjswan13@gmail.com>
2024-02-16 10:54:44 -0800

    contact list rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useState } from 'react';
-import ContactList from './Components/ContactList.jsx';
+import ContactList from './Components/contactList.jsx';
 import SelectedContact from "./Components/SelectedContact.jsx";
 
 export default function App() {
